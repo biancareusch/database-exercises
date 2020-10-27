4bf8878bda097c1827b0f1144d0758644edfa69e
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-27 11:05:27 -0500

    added album_seeder and select statements for exercise 3

---
+++ select_exercises.sql
@@ -0,0 +1,12 @@
+SELECT 'All Albums from Pink Floyd' AS 'Info'
+SELECT name FROM albums WHERE artist = 'Pink Floyd'
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info'
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band'
+SELECT 'The genre for Nevermind' AS 'Info'
+SELECT genre FROM albums WHERE name = 'Nevermind'
+SELECT 'albums were released in the 1990s' AS 'Info'
+SELECT name FROM albums WHERE release > '1990'
+SELECT 'albums had less than 20 million certified sales' AS 'Info'
+SELECT name FROM albums WHERE sales < 20
+SELECT 'All the albums with a genre of "Rock"' AS 'Info'
+SELECT name FROM albums WHERE genre = 'Rock'
