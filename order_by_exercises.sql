43c8111725ff15799987c2c8196e0883be06fce6
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-28 15:15:01 -0500

    ordered first names by first_name

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,8 @@
 USE employees;
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena','Vidya','Maya')
+ORDER BY first_name ASC;
 
 SELECT * FROM employees WHERE last_name LIKE '%E';
 
