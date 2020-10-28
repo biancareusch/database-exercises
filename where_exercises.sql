d8f7f76bb7199e41b32bd8be751aa024f1fbdab9
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-28 14:58:36 -0500

    updates first statement to use OR

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,3 +8,8 @@ SELECT * FROM employees WHERE hire_date LIKE '199%';
 SELECT * FROM employees WHERE birth_date = '%-12-25';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name =  'Maya';
+
