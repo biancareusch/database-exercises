0a72b147a8bc86536fa6d6c4ef56952c763ddc8c
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-28 14:56:16 -0500

    added singel quotes to fix error

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,7 +3,7 @@ SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
 
 SELECT * FROM employees WHERE last_name LIKE '%E';
 
-SELECT * FROM employees WHERE hire_date < 1990;
+SELECT * FROM employees WHERE hire_date LIKE '199%';
 
 SELECT * FROM employees WHERE birth_date = '%-12-25';
 
