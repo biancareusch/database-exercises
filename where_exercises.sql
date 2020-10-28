bc6ed073ca8c4c7dcbe01745fd55c2aa4b91656b
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-28 14:45:29 -0500

    finished exercise 6

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,10 @@
 USE employees;
 SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
 
-SELECT * FROM employees Where last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE '%E';
 
+SELECT * FROM employees WHERE hire_date < 1990;
+
+SELECT * FROM employees WHERE birth_date = '%-12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
