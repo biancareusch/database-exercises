4661e38a124d9fce7d8be58af20d074cdea4ae9c
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-28 15:00:28 -0500

    updated statement to show everybody with those names who is male

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,3 +13,4 @@ SELECT * FROM employees WHERE first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name =  'Maya';
 
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'm';
